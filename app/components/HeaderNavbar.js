c9b12b89faeba4eb5993c7c422772bc58914d79a
SKRTEEEEEE <adanreh.m@gmail.com>
2024-02-29 13:25:14 +0100

    logos-navbar
    Put logos in the navbar
    Allow see navbar in mobile, and tablet
    In mobile, only show logos

--- app/components/HeaderNavbar.js
+++ app/components/HeaderNavbar.js
@@ -1,25 +1,28 @@
+import Image from "next/image";
 import { links } from "../const/navbar";
 import Link from 'next/link'
 
 export default function HeaderComponent() {
 
 
-  
-  
+
+
   return (
-    
-    
-    <div className="flex xl:gap-12 justify-around">
-      <h1 className="">Blog Template</h1>
+
+
+    <div className="flex xl:gap-12 sm:gap-6 gap-2 justify-around">
+      <h1 className="px-4">Blog Template</h1>
+      {/* <Image src="/icons/seo-icon--neon-website--cyberpunk-colors.svg" width={30} height={30}/> */}
       {links.map((link) => (
-        <span key={link.nombre} className="sm:p-4 ">
-          <Link href={link.enlace} className="">
-            
-            <span className="">{link.nombre}</span><span className="">{link.logo}</span>
+        <span key={link.nombre} className="">
+          <Link href={link.enlace} className="xl:p-4 xl:w-1/10">
+
+            <span className="sm:flex  hidden">{link.nombre}</span>
+            <Image src={link.img} width={50} height={50}/>
           </Link>
         </span>
       ))}
     </div>
- 
+
   );
 }
--- app/const/navbar.js
+++ app/const/navbar.js
@@ -2,37 +2,44 @@ export const links = [
     {
         nombre: "Main",
         enlace: "/",
-        logo: "🏠"
+        logo: "🏠",
+        img: "/icons/initial-page-icon.svg"
     },
     {
         nombre: "Products",
         enlace: "/products",
-        logo: "🎶"
+        logo: "🎶",
+        img: "/icons/products-icon.svg"
     },
     {
         nombre: "Reviews",
         enlace: "/reviews",
-        logo: "👍"
+        logo: "👍",
+        img: "/icons/reviews-icon.svg"
     },
     {
         nombre: "News",
         enlace: "/news",
-        logo: "✍️📰"
+        logo: "✍️📰",
+        img: "/icons/news-icon.svg"
     },
     {
         nombre: "Contacts",
         enlace:  "/contacts",
-        logo: "ℹ️📃"
+        logo: "ℹ️📃",
+        img: "/icons/contact-icon.svg"
     },
 
     {
         nombre: "Privacy",
         enlace: "/privacy",
-        logo: "✍️📰"
+        logo: "✍️📰",
+        img: "/icons/privacy-icon.svg"
     },
     {
         nombre: "SEO",
         enlace: "/SEO",
-        logo: "👌"
+        logo: "👌",
+        img: "/icons/seo-icon.svg"
     },
 ]
