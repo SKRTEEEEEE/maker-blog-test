4fe9705f9ab26704209935083bab4a7e1ee2af51
SKRTEEEEEE <adanreh.m@gmail.com>
2024-05-27 08:57:45 +0200

    test(sidebar): Collapsed sidebar showtest

--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 const SideBar = () => {
     const [open, setOpen] = useState(false);
     return (
-        <div className={open?`h-screen absolute top-0 left-0 bg-blue-500 flex flex-col w-24`:"h-screen absolute top-0 left-0 bg-blue-500 flex flex-col w-8"}>
+        <div className={open?`h-screen absolute top-0 right-0 bg-blue-500 flex flex-col w-24`:"h-screen absolute top-0 right-0 bg-blue-500 flex flex-col w-8"}>
             <button className="relative top-0" onClick={()=>setOpen(!open)}>🔒</button><ul className='pt-12'>
                 <li> <span></span><h2 className={!open&&"hidden"}>Hola mundo</h2> </li>
                 <li> <span></span><h2 className={!open&&"hidden"}>Hola mundo</h2> </li>
